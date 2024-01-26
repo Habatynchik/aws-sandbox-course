23f3bdbd7709a6e93809a996f6e7aff479ef1706
Nikita <Habatynchik@gmail.com>
2024-01-26 11:08:44 -0800

    task09

---
+++ task09/jsrc/main/java/com/task09/model/Weather.java
@@ -0,0 +1,37 @@
+package com.task09.model;
+
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@DynamoDBTable(tableName = "cmtr-c04fb6ad-Weather-test")
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Weather {
+    private String id;
+    private Forecast forecast;
+
+    @DynamoDBHashKey(attributeName = "id")
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @DynamoDBAttribute(attributeName = "forecast")
+    public Forecast getForecast() {
+        return forecast;
+    }
+
+    public void setForecast(Forecast forecast) {
+        this.forecast = forecast;
+    }
+}
