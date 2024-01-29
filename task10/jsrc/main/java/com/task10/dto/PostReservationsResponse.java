539a3a6ff198a316a84b6759dd6bb1840c03b489
Nikita <Habatynchik@gmail.com>
2024-01-29 20:42:39 +0200

    task10

---
+++ task10/jsrc/main/java/com/task10/dto/PostReservationsResponse.java
@@ -0,0 +1,10 @@
+package com.task10.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class PostReservationsResponse {
+    private String reservationId;
+}
---
+++ task10/jsrc/main/java/com/task10/dto/PostTablesResponse.java
@@ -0,0 +1,12 @@
+package com.task10.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class PostTablesResponse {
+    private int id;
+}
